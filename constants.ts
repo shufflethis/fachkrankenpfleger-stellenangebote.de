ca9f129e0516b691e22ffc4f2d51065e2306026f
Autoblogger <dev@famefact.de>
2026-03-04 13:32:23 +0100

    chore: update job listings from external sources (HR-JobScraper)
    25 jobs injected from: Arbeitsagentur, Indeed, StepStone, meinestadt.de

--- constants.ts
+++ constants.ts
@@ -3,32 +3,252 @@ import { JobOffer } from './types';
 export const MOCK_JOBS: JobOffer[] = [
   {
     id: '1',
-    title: 'Fachkrankenpfleger Intensivpflege (m/w/d)',
-    hospital: 'Charité – Universitätsmedizin Berlin',
-    location: 'Berlin',
-    salary: '3.800€ - 4.600€',
-    tags: ['Intensivstation', 'Beatmung'],
-    description: 'Fachkrankenpfleger Intensivpflege (m/w/d)',
-    postedDate: 'Vor 2 Tagen'
+    title: 'Kundenberater (m/w/d)',
+    hospital: 'Boerger Tankservice GmbHSofort-Bewerbung',
+    location: 'München',
+    type: 'Vollzeit',
+    description: 'Kundenberater (m/w/d)',
+    tags: [],
+    postedDate: '2026-03-04'
   },
   {
     id: '2',
-    title: 'Fachkrankenpfleger Anästhesie (m/w/d)',
-    hospital: 'Universitätsklinikum München',
-    location: 'München',
-    salary: '3.600€ - 4.400€',
-    tags: ['Anästhesie', 'OP'],
-    description: 'Fachkrankenpfleger Anästhesie (m/w/d)',
-    postedDate: 'Neu'
+    title: 'Fahrer und Messgehilfe (m/w/d)',
+    hospital: 'Landratsamt Schwäbisch Hall',
+    location: 'Crailsheim',
+    type: 'Vollzeit',
+    description: 'Fahrer und Messgehilfe (m/w/d)',
+    tags: [],
+    postedDate: '2026-03-04'
   },
   {
     id: '3',
-    title: 'Pflegefachkraft Intensivstation (m/w/d)',
-    hospital: 'Universitätsklinikum Hamburg-Eppendorf',
-    location: 'Hamburg',
-    salary: '3.500€ - 4.200€',
-    tags: ['Intensivpflege', 'Weiterbildung'],
-    description: 'Pflegefachkraft Intensivstation (m/w/d)',
-    postedDate: 'Vor 1 Woche'
+    title: 'Fahrer und Messgehilfe (m/w/d)',
+    hospital: 'Landratsamt Schwäbisch Hall',
+    location: 'Schwäbisch Hall',
+    type: 'Vollzeit',
+    description: 'Fahrer und Messgehilfe (m/w/d)',
+    tags: [],
+    postedDate: '2026-03-04'
+  },
+  {
+    id: '4',
+    title: 'Hauswirtschaftliche Leitung (m/w/d) für unser Kinderhaus in Teil- oder Vollzeit',
+    hospital: 'Kinder- und Jugendhilfe Weidenkorb',
+    location: 'Löhne',
+    type: 'Vollzeit',
+    description: 'Hauswirtschaftliche Leitung (m/w/d) für unser Kinderhaus in Teil- oder Vollzeit',
+    tags: [],
+    postedDate: '2026-03-04'
+  },
+  {
+    id: '5',
+    title: 'Hauswirtschaftliche Leitung (m/w/d) für unser Kinderhaus in Teil- oder Vollzeit',
+    hospital: 'Kinder- und Jugendhilfe Weidenkorb',
+    location: 'Espelkamp',
+    type: 'Vollzeit',
+    description: 'Hauswirtschaftliche Leitung (m/w/d) für unser Kinderhaus in Teil- oder Vollzeit',
+    tags: [],
+    postedDate: '2026-03-04'
+  },
+  {
+    id: '6',
+    title: 'Physiotherapeut*in (m/w/d) mit Zusatzqualifikation',
+    hospital: 'Reha-Zentrum Gesundheit Nordhessen GmbH',
+    location: 'Kassel',
+    type: 'Vollzeit',
+    description: 'Physiotherapeut*in (m/w/d) mit Zusatzqualifikation',
+    tags: [],
+    postedDate: '2026-03-04'
+  },
+  {
+    id: '7',
+    title: 'Fachkrankenpfleger - OP (m/w/d)',
+    hospital: 'Impuls Personal GmbH Filiale Köln',
+    location: 'Köln',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger - OP (m/w/d)',
+    tags: ['Fachkrankenschwester/-pfleger - Operationsdienst'],
+    postedDate: '2026-03-04'
+  },
+  {
+    id: '8',
+    title: 'Fachkrankenpfleger - OP (m/w/d)',
+    hospital: 'Impuls Personal GmbH Filiale Köln',
+    location: 'Bergisch Gladbach',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger - OP (m/w/d)',
+    tags: ['Fachkrankenschwester/-pfleger - Operationsdienst'],
+    postedDate: '2026-03-03'
+  },
+  {
+    id: '9',
+    title: 'Fachkrankenpfleger/in - Psychiatrie',
+    hospital: 'Pflege und Sozialdienste Dornbirn GmbH',
+    location: 'Dornbirn',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger/in - Psychiatrie',
+    tags: ['Fachkrankenpfleger/in - Psychiatrie'],
+    postedDate: '2026-03-03'
+  },
+  {
+    id: '10',
+    title: 'Fachkrankenpfleger/in - Psychiatrie',
+    hospital: 'Bgld. Pflegeheim Betriebs-G es.mbH',
+    location: 'Neudörfl',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger/in - Psychiatrie',
+    tags: ['Fachkrankenpfleger/in - Psychiatrie'],
+    postedDate: '2026-03-03'
+  },
+  {
+    id: '11',
+    title: 'Fachkrankenpfleger (m/w/d) - Intensiv',
+    hospital: 'Unique Personal Service SüdWest GmbH',
+    location: 'Bonn',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger (m/w/d) - Intensiv',
+    tags: ['Fachkrankenpfleger/in - Intensivpflege/Anästhesie'],
+    postedDate: '2026-03-02'
+  },
+  {
+    id: '12',
+    title: 'Fachkrankenpfleger (m/w/d) Onkologie',
+    hospital: 'Med-Spezialisten GmbH',
+    location: 'Berlin',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger (m/w/d) Onkologie',
+    tags: ['Fachkrankenpfleger/in - Onkologie'],
+    postedDate: '2026-03-02'
+  },
+  {
+    id: '13',
+    title: 'Fachkrankenpfleger (m/w/d)',
+    hospital: 'KCS Medical GmbH',
+    location: 'Heidelberg, Neckar',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger (m/w/d)',
+    tags: ['Fachkrankenpfleger/in - Notfallpflege'],
+    postedDate: '2026-02-27'
+  },
+  {
+    id: '14',
+    title: 'Fachkrankenpfleger (m/w/d)',
+    hospital: 'KCS Medical GmbH',
+    location: 'Mannheim',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger (m/w/d)',
+    tags: ['Fachkrankenpfleger/in - Notfallpflege'],
+    postedDate: '2026-02-27'
+  },
+  {
+    id: '15',
+    title: 'Fachkrankenpfleger (m/w/d)',
+    hospital: 'KCS Medical GmbH',
+    location: 'Mainz am Rhein',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger (m/w/d)',
+    tags: ['Fachkrankenpfleger/in - Notfallpflege'],
+    postedDate: '2026-02-27'
+  },
+  {
+    id: '16',
+    title: 'Fachkrankenpfleger (m/w/d)',
+    hospital: 'KCS Medical GmbH',
+    location: 'Frankfurt am Main',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger (m/w/d)',
+    tags: ['Fachkrankenpfleger/in - Notfallpflege'],
+    postedDate: '2026-02-27'
+  },
+  {
+    id: '17',
+    title: 'Fachkrankenpfleger (m/w/d)',
+    hospital: 'KCS Medical GmbH',
+    location: 'Berlin',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger (m/w/d)',
+    tags: ['Fachkrankenpfleger/in - Notfallpflege'],
+    postedDate: '2026-02-27'
+  },
+  {
+    id: '18',
+    title: 'Fachkrankenpfleger Intensiv (m/w/d)',
+    hospital: 'KCS Medical GmbH',
+    location: 'München',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger Intensiv (m/w/d)',
+    tags: ['Fachkrankenpfleger/in - Intensivpflege/Anästhesie'],
+    postedDate: '2026-02-27'
+  },
+  {
+    id: '19',
+    title: 'Fachkrankenpfleger (m/w/d) für Intensivpflege',
+    hospital: 'KCS Medical GmbH',
+    location: 'Köln',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger (m/w/d) für Intensivpflege',
+    tags: ['Fachkrankenpfleger/in - Intensivpflege/Anästhesie'],
+    postedDate: '2026-02-27'
+  },
+  {
+    id: '20',
+    title: 'Fachkrankenpfleger Intensiv (m/w/d)',
+    hospital: 'KCS Medical GmbH',
+    location: 'Dresden',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger Intensiv (m/w/d)',
+    tags: ['Fachkrankenpfleger/in - Intensivpflege/Anästhesie'],
+    postedDate: '2026-02-27'
+  },
+  {
+    id: '21',
+    title: 'Fachkrankenpfleger Notfallpflege (m/w/d)',
+    hospital: 'KCS Medical GmbH',
+    location: 'München',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger Notfallpflege (m/w/d)',
+    tags: ['Fachkrankenpfleger/in - Notfallpflege'],
+    postedDate: '2026-02-27'
+  },
+  {
+    id: '22',
+    title: 'Fachkrankenpfleger Anästhesie (m/w/d)',
+    hospital: 'CONFIANZA GmbH Personalmanagement',
+    location: 'Augsburg, Bayern',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger Anästhesie (m/w/d)',
+    tags: ['Fachkrankenpfleger/in - Intensivpflege/Anästhesie'],
+    postedDate: '2026-02-27'
+  },
+  {
+    id: '23',
+    title: 'Fachkrankenpfleger/in - Psychiatrie',
+    hospital: 'A.ö.Krankenhaus St.Vinzenz Betriebsges.m.b.H.',
+    location: 'Zams',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger/in - Psychiatrie',
+    tags: ['Fachkrankenpfleger/in - Psychiatrie'],
+    postedDate: '2026-02-25'
+  },
+  {
+    id: '24',
+    title: 'Fachkrankenpfleger (m/w/d) Psychosomatik',
+    hospital: 'Med-Spezialisten GmbH',
+    location: 'Bühl, Baden',
+    type: 'Vollzeit',
+    description: 'Fachkrankenpfleger (m/w/d) Psychosomatik',
+    tags: ['Fachkrankenpfleger/in - Psychiatrie'],
+    postedDate: '2026-02-22'
+  },
+  {
+    id: '25',
+    title: 'Fachkrankenschwester/-pfleger - Operationsdienst',
+    hospital: 'Mein Zentrum Dr. Klug & Steinbrecher OG',
+    location: 'Hausleiten',
+    type: 'Vollzeit',
+    description: 'Fachkrankenschwester/-pfleger - Operationsdienst',
+    tags: ['Fachkrankenschwester/-pfleger - Operationsdienst'],
+    postedDate: '2026-02-20'
   }
 ];
